453367e3f6ec8f7c2daf94b09854c3e93de75deb
tayFboi <88890243+tayFboi@users.noreply.github.com>
2021-08-13 20:10:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'may th lon',
+    introDesc: `djt me tui m,
+    btnIntro: 'nhan vo day',
+    title: 'dak dak',
+    desc: 'hay chon 1 cai minh thay đúng :)) ',
+    btnYes: 'lon xiu da den cac be',
+    btnNo: 'xiu dep trai cu to',
+    question:'m muon djt con phuong dung k',
+    btnReply: 'gui di nhanh len',
+    reply: 'dung vay t muon djt lo djt con phuong',
+    mess: 'ah con cho mat day',
+    messDesc: 'm dc lam',
+    btnAccept: 'dak dak',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
